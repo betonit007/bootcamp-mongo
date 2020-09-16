346c77a0fc2fb50e2d4d37fe166da3ca4c7ac6b1
betonit007 <timnagorski@hotmail.com>
2020-09-15 21:01:04 -0400

    deploy

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+console.log(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
